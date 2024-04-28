69e78689e102f289632aa00b826cd1f9fe09f063
Artur <metallist15431543@gmail.com>
2024-04-28 10:28:03 +0300

    HW5 finished

--- src/s2-homeworks/hw05/pages/JuniorPlus.tsx
+++ src/s2-homeworks/hw05/pages/JuniorPlus.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 // import HW10 from '../../hw10/HW10'
 // import HW11 from '../../hw11/HW11'
 // import HW12 from '../../hw12/HW12'
@@ -6,17 +6,17 @@ import React from 'react'
 // import HW14 from '../../hw14/HW14'
 // import HW15 from '../../hw15/HW15'
 
-function JuniorPlus() {
-    return (
-        <div id={'hw5-page-junior-plus'}>
-            {/*<HW10 />*/}
-            {/*<HW11 />*/}
-            {/*<HW12 />*/}
-            {/*<HW13 />*/}
-            {/*<HW14 />*/}
-            {/*<HW15 />*/}
-        </div>
-    )
+export function JuniorPlus() {
+  return (
+    <div id={"hw5-page-junior-plus"}>
+      {/*<HW10 />*/}
+      {/*<HW11 />*/}
+      {/*<HW12 />*/}
+      {/*<HW13 />*/}
+      {/*<HW14 />*/}
+      {/*<HW15 />*/}
+    </div>
+  );
 }
 
-export default JuniorPlus
+export default JuniorPlus;
