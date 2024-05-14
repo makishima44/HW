a59a2dc54667137721ce7b7b73c1d172e50f77b6
Artur <metallist15431543@gmail.com>
2024-05-14 15:56:40 +0300

    HW8 finished

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,27 @@
-import {UserType} from '../HW8'
+import { UserType } from "../HW8";
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | { type: "sort"; payload: "up" | "down" }
+  | { type: "check"; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
+export const homeWorkReducer = (
+  state: UserType[],
+  action: ActionType
+): UserType[] => {
+  // need to fix any
+  switch (action.type) {
+    case "sort": {
+      const copyState = [...state].sort(
+        action.payload === "up"
+          ? (a, b) => a.name.localeCompare(b.name)
+          : (a, b) => b.name.localeCompare(a.name)
+      );
+      return copyState;
     }
-}
+    case "check": {
+      return [...state.filter((u) => u.age > action.payload)];
+    }
+    default:
+      return state;
+  }
+};
